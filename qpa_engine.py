44b9bb3cad545d488df08949f4a0dbd14d92fa18
Jacky11 <2116403107@qq.com>
2025-10-28 20:34:53 -0400

    added GPA

--- qpa_engine.py
+++ qpa_engine.py
@@ -1,17 +1,21 @@
 def calculateQPA(Grades):
     # We assume the list of grades is like [(units, letter grade), ...]
     totalUnits = 0
+    totalPoints = 0
     totalQualityPoints = 0
     for each in Grades:
         units, letterGrade = each
-        qualityPoints = letterGradeToQualityPoints(letterGrade)
-        totalUnits += units
-        totalQualityPoints += units * qualityPoints
-    QPA = round(totalQualityPoints / totalUnits, 2)
-    print(QPA)
-    return QPA
+        points = letterGradeToPoints(letterGrade)
+        qualityPoints = points * units
 
-def letterGradeToQualityPoints(letterGrade):
+        totalPoints += points
+        totalUnits += units
+        totalQualityPoints += qualityPoints
+    QPA = round(totalQualityPoints / totalUnits, 2)
+    GPA = round(totalPoints/len(Grades), 2)
+    return {"QPA": QPA, "totalUnits": totalUnits, "totalQualityPoints": totalQualityPoints, "GPA": GPA}
+
+def letterGradeToPoints(letterGrade):
     gradeDict = {
         "A":4.0,
         "B":3.0,
@@ -20,4 +24,4 @@ def letterGradeToQualityPoints(letterGrade):
     }
     return gradeDict.get(letterGrade, 0.0)
 
-calculateQPA([ (12, "A"), (9, "C"), (9, "A"), (9, "A") ])
+calculateQPA([ (12, "A"), (9, "B"), (9, "A"), (9, "A") ])
