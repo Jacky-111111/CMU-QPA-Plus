68f4a2d911c9c931643270752b1ca5f7559df0d3
Jacky11 <2116403107@qq.com>
2025-11-07 20:19:23 -0500

    changed portal

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ def calculate_qpa(req: QpaRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5001)
+    uvicorn.run("main:app", host="0.0.0.0", port=5002)
 
     
